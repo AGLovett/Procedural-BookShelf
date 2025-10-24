9d8c12ab2ebd252ff5a7e41724f1b3b425303372
Avery Lovett <aaverygrace@gmail.com>
2025-10-24 16:15:32 -0500

    pseudo code and imports

---
+++ ProceduralBookShelf.py
@@ -0,0 +1,26 @@
+#import maya.cmds as cmds
+#import random
+#import math 
+#from Pyside import Qtcore 
+
+# generate_bookshelf
+# make a group for whole shelf
+# make the shelf 
+# Add books (books match prams of shelf)
+# return
+
+
+# create_shelf 
+# generate the whole shelf
+# add wood color 
+
+# add_books 
+# calculate the amount of books that can fit
+# add books till reaches max amount space
+# book prams (random height, width, depth)
+# put books next to eachother 
+# put color on books randomly 
+
+# create_book
+# create book
+# add color
