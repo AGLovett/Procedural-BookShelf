6dcf1e71e4d70a101e6efa46c51e9b04c05481f9
Avery Lovett <aaverygrace@gmail.com>
2025-10-24 16:44:07 -0500

    creating main shelf

--- ProceduralBookShelf.py
+++ ProceduralBookShelf.py
@@ -1,9 +1,18 @@
-#import maya.cmds as cmds
-#import random
-#import math 
-#from Pyside import Qtcore 
+import maya.cmds as cmds
+import random
+import math 
+from Pyside import Qtcore 
 
-# generate_bookshelf
+class BookshelfGen:
+
+    def create_shelf(self, params):
+        width = params['width']
+        height = params['height']
+        depth = params['shelf_depth']
+        num_shelves = params['num_shelves']
+   
+
+
 # make a group for whole shelf
 # make the shelf 
 # Add books (books match prams of shelf)
